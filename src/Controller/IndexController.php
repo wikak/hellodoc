91cc2ac2a567710806777ebb5ae929161ae2eb7e
Wilfried KAKOU <w.kakou@panelyscash.com>
2023-05-21 15:05:49 +0000

    prise en main

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,7 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
+    /**
+     * @Route("/", name="app_index")
+     */
     public function index(): Response
     {
         return $this->render('index/index.html.twig', [
